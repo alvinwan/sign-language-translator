0f48b705f93260cbb839a7f2d94af89089f3889e
Alvin Wan <alvinwan1@gmail.com>
2019-11-29 04:12:02 -0800

    cleaned up step 5, camera

--- src/step_5_camera.py
+++ src/step_5_camera.py
@@ -12,20 +12,25 @@ def center_crop(frame):
 
 
 def main():
+    # constants
+    index_to_letter = list('ABCDEFGHIKLMNOPQRSTUVWXY')
+    mean = 0.485 * 255.
+    std = 0.229 * 255.
+
     # create runnable session with exported model
     ort_session = ort.InferenceSession("signlanguage.onnx")
-    index_to_letter = list('ABCDEFGHIKLMNOPQRSTUVWXY')
 
     cap = cv2.VideoCapture(0)
     while True:
         # Capture frame-by-frame
         ret, frame = cap.read()
+
+        # preprocess data
         frame = center_crop(frame)
-
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        x = (frame - 0.485) / 0.229
-
         x = cv2.resize(x, (28, 28))
+        x = (x - mean) / std
+
         x = x.reshape(1, 1, 28, 28).astype(np.float32)
         y = ort_session.run(None, {'input': x})[0]
 
@@ -33,7 +38,7 @@ def main():
         letter = index_to_letter[int(index)]
 
         cv2.putText(frame, letter, (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), thickness=2)
-        cv2.imshow("you", frame)
+        cv2.imshow("Sign Language Translator", frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
